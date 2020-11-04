164652056410f8b7c684c6c3a4c7330031cbfdaf
Artem <example@gmail.com>
2020-11-04 13:06:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//1 Переменные
+//camelCase
+// Это код Артёма для обучения программированию.
+const firstName = 'Artem'
+const lastName = 'Knyazev'
+let age = 12
+name = 'Tema'
+console.log(name)
+
+const _private = 'private'
+const $ = 'some value'
+
+const withNum5 = '5'
+
+// это выввод в консоль результатов
+console.log('Имя человека: ' + firstName)
