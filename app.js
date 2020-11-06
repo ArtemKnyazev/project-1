a7ef720d6e9be270167f35b956d785dc2ce9b7c5
Artem <example@gmail.com>
2020-11-06 20:05:31 +0300

    Второй коомит Артёма

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-//1 Переменные
-//camelCase
-// Это код Артёма для обучения программированию.
-const firstName = 'Artem'
-const lastName = 'Knyazev'
-let age = 12
-name = 'Tema'
-console.log(name)
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const _private = 'private'
-const $ = 'some value'
+const a = prompt('Один из последних просмотренный фильмов?',''),
+b = prompt('На сколько оцените его?',''),
+c = prompt('Один из последних просмотренный фильмов?',''),
+d = prompt('На сколько оцените его?','');
 
-const withNum5 = '5'
-
-// это выввод в консоль результатов
-console.log('Имя человека: ' + firstName)
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
